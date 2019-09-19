6714a09b0d584808ac41aecbd309fd1bbf60f75a
alonso1298 <alonsosagrerogranados@gmail.com>
2019-09-19 17:36:48 -0500

    problema 1.1 al 1.2

---
+++ libro/problemas_resueltos/capitulo1/problema1_2.py
@@ -0,0 +1,6 @@
+BASE = float(input("Dame la base del triangulo:"))
+ALTU = float(input("Dame la altura del triangulo:"))
+SUP = (BASE * ALTU)/2
+print(f"El area de este triangulo es { SUP } dado que la altura es {ALTU} la base es {BASE}")
+
+
